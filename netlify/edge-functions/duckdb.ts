ad5c8327a1df989d32fa8196b5430049d2aa6b7a
mnsrulz <manishrawat4u@gmail.com>
2023-12-17 21:20:27 +0000

    import deno

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -1,4 +1,5 @@
 import * as duckdb from "https://esm.sh/@duckdb/duckdb-wasm@1.28.0";
+import { Int } from "https://esm.sh/v135/apache-arrow@13.0.0/type.js";
 const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
 const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
 const worker_url = new URL(bundle.mainWorker!, import.meta.url).href;
@@ -15,11 +16,11 @@ BigInt.prototype.toJSON = function () { return this.toString() }
 
 export default async function handler(req: Request, context: Context) {
     const conn = await db.connect();
-    const arrowResult = await conn.query(`SELECT COUNT(*) AS total_count
+    const arrowResult = await conn.query<{total_count: Int}>(`SELECT COUNT(*) AS total_count
                                             FROM 'taxi_2019_04.parquet'
                                             WHERE pickup_at BETWEEN '2019-04-15' AND '2019-04-20'`)
+                                      //arrowResult.get(0)?.total_count      
     const result = arrowResult.toArray().map((row) => row.toJSON());
-
     return Response.json(result[0]);
 }
 
