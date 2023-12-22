b11d09dfe6467e59fc425b6a2a50563fac69406f
mnsrulz <manishrawat4u@gmail.com>
2023-12-22 01:46:29 +0000

    added the duckdb

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -1,18 +1,18 @@
-// import { getJsDelivrBundles, selectBundle, ConsoleLogger, AsyncDuckDB } from "https://esm.sh/@duckdb/duckdb-wasm@1.28.0";
-// import { Int } from "https://esm.sh/v135/apache-arrow@13.0.0/type.js";
-// const JSDELIVR_BUNDLES = getJsDelivrBundles();
-// const bundle = await selectBundle(JSDELIVR_BUNDLES);
-// const worker_url = new URL(bundle.mainWorker!, import.meta.url).href;
+import { getJsDelivrBundles, selectBundle, ConsoleLogger, AsyncDuckDB } from "https://esm.sh/@duckdb/duckdb-wasm@1.28.0";
+import { Int } from "https://esm.sh/v135/apache-arrow@13.0.0/type.js";
+const JSDELIVR_BUNDLES = getJsDelivrBundles();
+const bundle = await selectBundle(JSDELIVR_BUNDLES);
+const worker_url = new URL(bundle.mainWorker!, import.meta.url).href;
 
-// const worker = new Worker(worker_url, { type: 'module' })
-// const logger = new ConsoleLogger();
-// const db = new AsyncDuckDB(logger, worker);
-// await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-// URL.revokeObjectURL(worker_url);
+const worker = new Worker(worker_url, { type: 'module' })
+const logger = new ConsoleLogger();
+const db = new AsyncDuckDB(logger, worker);
+await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+URL.revokeObjectURL(worker_url);
 
-// const res = await fetch('https://github.com/cwida/duckdb-data/releases/download/v1.0/taxi_2019_04.parquet');
-// await db.registerFileBuffer('taxi_2019_04.parquet', new Uint8Array(await res.arrayBuffer()));
-// BigInt.prototype.toJSON = function () { return this.toString() }
+const res = await fetch('https://github.com/cwida/duckdb-data/releases/download/v1.0/taxi_2019_04.parquet');
+await db.registerFileBuffer('taxi_2019_04.parquet', new Uint8Array(await res.arrayBuffer()));
+BigInt.prototype.toJSON = function () { return this.toString() }
 
 // export default async function handler(req: Request, context: Context) {
 //     const conn = await db.connect();
@@ -30,6 +30,13 @@
 
 
 Deno.serve(async (req) => {
+    const conn = await db.connect();
+    const arrowResult = await conn.query<{total_count: Int}>(`SELECT COUNT(*) AS total_count
+                                            FROM 'taxi_2019_04.parquet'
+                                            WHERE pickup_at BETWEEN '2019-04-15' AND '2019-04-20'`)
+                                      //arrowResult.get(0)?.total_count      
+    const result = arrowResult.toArray().map((row) => row.toJSON());
+    //return Response.json(result[0]);
     console.log("Method:", req.method);
   
     const url = new URL(req.url);
