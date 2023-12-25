0a2f6f6aa82314e75f5e4fe70cb62ce04598fba2
mnsrulz <manishrawat4u@gmail.com>
2023-12-25 19:56:27 +0000

    adding raw query

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -1,20 +1,5 @@
-// @deno-types="https://esm.sh/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.d.ts"
-import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
 import { Int32 } from 'https://esm.sh/v135/apache-arrow@13.0.0/type.js';
-const logger = new ConsoleLogger();
-const JSDELIVR_BUNDLES = getJsDelivrBundles();
-const db = await createDuckDB(JSDELIVR_BUNDLES, logger, DEFAULT_RUNTIME);
-await db.instantiate(() => { });
-
-const arrayBuffer = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet')    //let's initialize the data set in memory
-    .then(r => r.arrayBuffer());
-db.registerFileBuffer('db.parquet', new Uint8Array(arrayBuffer));
-
-//HTTP paths are not supported due to xhr not available in deno.
-//db.registerFileURL('db.parquet', 'https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet', DuckDBDataProtocol.HTTP, false);
-
-const conn = db.connect();
-
+import { conn } from '../../services/db.ts'
 export default function handler() {
     const arrowResult = conn.query<{ total_count: Int32 }>(`SELECT COUNT(*) AS total_count FROM 'db.parquet'`);
     return Response.json({
---
+++ netlify/edge-functions/raw.ts
@@ -0,0 +1,13 @@
+import { conn } from '../../services/db.ts'
+export default function handler(req: Request) {
+    const q = new URL(req.url).searchParams.get('q');
+    if(!q) throw new Error(`empty query provided. Use with ?q=YOUR_QUERY`)
+    const arrowResult = conn.query(q);
+    return Response.json({
+        total_count: arrowResult.get(0)?.total_count
+    });
+}
+
+export const config = {
+    path: "/raw"
+};
