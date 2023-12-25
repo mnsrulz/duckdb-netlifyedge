79081938a9e881ec520eeac28eb1a0623a79cc6e
mnsrulz <manishrawat4u@gmail.com>
2023-12-25 12:17:07 -0500

    cleanup

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -1,20 +1,16 @@
-import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
-import { getQuery } from "https://deno.land/x/oak@v12.6.1/helpers.ts";
 const logger = new ConsoleLogger();
 const JSDELIVR_BUNDLES = getJsDelivrBundles();
-const ddb = await createDuckDB(JSDELIVR_BUNDLES, logger, DEFAULT_RUNTIME);
+const db = await createDuckDB(JSDELIVR_BUNDLES, logger, DEFAULT_RUNTIME);
 
-await ddb.instantiate(() => { });
+await db.instantiate(() => { });
 
 const res = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet');
-ddb.registerFileBuffer('db.parquet', new Uint8Array(await res.arrayBuffer()));
-const conn = ddb.connect();
+db.registerFileBuffer('db.parquet', new Uint8Array(await res.arrayBuffer()));
+const conn = db.connect();
 
 export default async function handler(req: Request, context: Context) {
-    const arrowResult = conn.query<{total_count: number}>(`SELECT COUNT(*) AS total_count
-                                              FROM 'db.parquet'
-                                              `);
+    const arrowResult = conn.query(`SELECT COUNT(*) AS total_count FROM 'db.parquet'`);
     const result = arrowResult.toArray().map((row: any) => row.toJSON());
     return Response.json(result);
 }
