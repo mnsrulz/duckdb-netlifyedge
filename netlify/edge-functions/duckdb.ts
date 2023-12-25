38477777b29c74df11fd631f68f0a59fc856dc22
mnsrulz <manishrawat4u@gmail.com>
2023-12-25 12:23:14 -0500

    trying http

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -1,12 +1,15 @@
-import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
+import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME, DuckDBDataProtocol } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
 const logger = new ConsoleLogger();
 const JSDELIVR_BUNDLES = getJsDelivrBundles();
 const db = await createDuckDB(JSDELIVR_BUNDLES, logger, DEFAULT_RUNTIME);
 
 await db.instantiate(() => { });
 
-const res = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet');
-db.registerFileBuffer('db.parquet', new Uint8Array(await res.arrayBuffer()));
+// const res = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet');
+// db.registerFileBuffer('db.parquet', new Uint8Array(await res.arrayBuffer()));
+
+db.registerFileURL('db.parquet', 'https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet', DuckDBDataProtocol.HTTP, false);
+
 const conn = db.connect();
 
 export default async function handler(req: Request, context: Context) {
