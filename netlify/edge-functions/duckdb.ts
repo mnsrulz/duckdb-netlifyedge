5b9822c5bead8d6b6313ed8b6b042eb3f1a766b1
mnsrulz <manishrawat4u@gmail.com>
2023-12-25 03:55:50 +0000

    allow esm

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -31,7 +31,8 @@
 //import pl from "npm:nodejs-polars";
 //import pl from "https://esm.sh/nodejs-polars";
 // @deno-types="https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-node-blocking.d.ts"
-import { createDuckDB, getJsDelivrBundles, ConsoleLogger, NODE_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-node-blocking.cjs';
+//import { createDuckDB, getJsDelivrBundles, ConsoleLogger, NODE_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-node-blocking.cjs';
+import { createDuckDB, getJsDelivrBundles, ConsoleLogger, NODE_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-node-blocking.cjs/+esm';
 
 const logger = new ConsoleLogger();
 const JSDELIVR_BUNDLES = getJsDelivrBundles();
