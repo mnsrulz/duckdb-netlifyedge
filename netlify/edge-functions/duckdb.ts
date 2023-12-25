ebf6d349c4b7881da53d4240c9c8ed1887ccbf34
mnsrulz <manishrawat4u@gmail.com>
2023-12-25 18:51:48 +0000

    cleanup

--- netlify/edge-functions/duckdb.ts
+++ netlify/edge-functions/duckdb.ts
@@ -1,23 +1,26 @@
-import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME, DuckDBDataProtocol } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
+// @deno-types="https://esm.sh/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.d.ts"
+import { createDuckDB, getJsDelivrBundles, ConsoleLogger, DEFAULT_RUNTIME } from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-browser-blocking.mjs/+esm';
+import { Int32 } from 'https://esm.sh/v135/apache-arrow@13.0.0/type.js';
 const logger = new ConsoleLogger();
 const JSDELIVR_BUNDLES = getJsDelivrBundles();
 const db = await createDuckDB(JSDELIVR_BUNDLES, logger, DEFAULT_RUNTIME);
-
 await db.instantiate(() => { });
 
-// const res = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet');
-// db.registerFileBuffer('db.parquet', new Uint8Array(await res.arrayBuffer()));
+const res = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet');    //let's initialize the data set in memory
+db.registerFileBuffer('db.parquet', new Uint8Array(await res.arrayBuffer()));
 
-db.registerFileURL('db.parquet', 'https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet', DuckDBDataProtocol.HTTP, false);
+//HTTP paths are not supported due to xhr not available in deno.
+//db.registerFileURL('db.parquet', 'https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet', DuckDBDataProtocol.HTTP, false);
 
 const conn = db.connect();
 
-export default async function handler(req: Request, context: Context) {
-    const arrowResult = conn.query(`SELECT COUNT(*) AS total_count FROM 'db.parquet'`);
-    const result = arrowResult.toArray().map((row: any) => row.toJSON());
-    return Response.json(result);
+export default function handler() {
+    const arrowResult = conn.query<{ total_count: Int32 }>(`SELECT COUNT(*) AS total_count FROM 'db.parquet'`);
+    return Response.json({
+        total_count: arrowResult.get(0)?.total_count
+    });
 }
 
-export const config: Config = {
-    path: "/duckdb",
+export const config = {
+    path: "/duckdb"
 };
