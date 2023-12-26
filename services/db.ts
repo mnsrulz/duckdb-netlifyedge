b86defd3c950261b492e008d677b7f2f0ed64577
mnsrulz <manishrawat4u@gmail.com>
2023-12-26 13:53:27 -0500

    up v1.1

--- services/db.ts
+++ services/db.ts
@@ -5,12 +5,12 @@ const JSDELIVR_BUNDLES = getJsDelivrBundles();
 const db = await createDuckDB(JSDELIVR_BUNDLES, logger, DEFAULT_RUNTIME);
 await db.instantiate(() => { });
 
-const arrayBuffer = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet')    //let's initialize the data set in memory
+const arrayBuffer = await fetch('https://github.com/mnsrulz/hpqdata/releases/download/v1.1/db.parquet')    //let's initialize the data set in memory
     .then(r => r.arrayBuffer());
 db.registerFileBuffer('db.parquet', new Uint8Array(arrayBuffer));
 
 //HTTP paths are not supported due to xhr not available in deno.
-//db.registerFileURL('db.parquet', 'https://github.com/mnsrulz/hpqdata/releases/download/v1.0/db.parquet', DuckDBDataProtocol.HTTP, false);
+//db.registerFileURL('db.parquet', 'https://github.com/mnsrulz/hpqdata/releases/download/v1.1/db.parquet', DuckDBDataProtocol.HTTP, false);
 
 export const conn = db.connect();
 
