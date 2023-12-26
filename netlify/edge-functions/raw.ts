d930932e7a58bfe06685847ab1148c0e0f0ed5c5
mnsrulz <manishrawat4u@gmail.com>
2023-12-25 19:44:35 -0500

    Update raw.ts

--- netlify/edge-functions/raw.ts
+++ netlify/edge-functions/raw.ts
@@ -1,14 +1,23 @@
 import { conn } from '../../services/db.ts'
 export default function handler(req: Request) {
-    const q = new URL(req.url).searchParams.get('q');
-    if(!q) throw new Error(`empty query provided. Use with ?q=YOUR_QUERY`)
-    const arrowResult = conn.query(q);
-    const result = arrowResult.toArray().map((row) => row.toJSON());
-    return Response.json(result, {
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
+    try {
+        const q = new URL(req.url).searchParams.get('q');
+        if(!q) throw new Error(`empty query provided. Use with ?q=YOUR_QUERY`)
+        const arrowResult = conn.query(q);
+        const result = arrowResult.toArray().map((row) => row.toJSON());
+        return Response.json(result, {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+    } catch (err) {
+         return Response.json({ error: "Unhandled error occurred", message: err.message }, {
+            status: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+    }
 }
 
 export const config = {
