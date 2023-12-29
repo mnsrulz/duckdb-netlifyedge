58379616d5bed30c8e3876f69757a468632cef3c
mnsrulz <manishrawat4u@gmail.com>
2023-12-29 12:12:01 -0500

    using the md5

--- netlify/edge-functions/raw.ts
+++ netlify/edge-functions/raw.ts
@@ -1,27 +1,31 @@
 import { conn } from '../../services/db.ts'
+import { md5 } from 'hash-wasm';
 //BigInt.prototype.toJSON = function() { return this.toString() }
 BigInt.prototype.toJSON = function() { return Number(this); }    //to keep them as numbers. Numbers have good range.
 const map = new Map<string, object>();
 const instanceId = crypto.randomUUID()
-export default function handler(req: Request) {
+export default async function handler(req: Request) {
     try {
         const q = new URL(req.url).searchParams.get('q');
         if(!q) throw new Error(`empty query provided. Use with ?q=YOUR_QUERY`)
-        if (map.get(q)) return Response.json(map.get(q), {
+        const qmd5 = await md5(q);
+        if (map.get(qmd5)) return Response.json(map.get(qmd5), {
           headers: {
             'Access-Control-Allow-Origin': '*',
             'x-read-from': 'cache-map',
-            'x-instance-id': instanceId
+            'x-instance-id': instanceId,
+            'x-md5': qmd5
           }
         });
         
         const arrowResult = conn.query(q);
         const result = arrowResult.toArray().map((row) => row.toJSON());
-        map.set(q, result);
+        map.set(qmd5, result);
         return Response.json(result, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
-                'x-instance-id': instanceId
+                'x-instance-id': instanceId,
+                'x-md5': qmd5
             }
         });
     } catch (err) {
