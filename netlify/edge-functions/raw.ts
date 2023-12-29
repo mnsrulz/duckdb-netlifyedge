5a866e646ce724529f56970fa95ea7b0f0bc002c
mnsrulz <manishrawat4u@gmail.com>
2023-12-29 11:52:21 -0500

    Update raw.ts

--- netlify/edge-functions/raw.ts
+++ netlify/edge-functions/raw.ts
@@ -1,12 +1,20 @@
 import { conn } from '../../services/db.ts'
 //BigInt.prototype.toJSON = function() { return this.toString() }
 BigInt.prototype.toJSON = function() { return Number(this); }    //to keep them as numbers. Numbers have good range.
+const map = new Map<string, object>();
 export default function handler(req: Request) {
     try {
         const q = new URL(req.url).searchParams.get('q');
         if(!q) throw new Error(`empty query provided. Use with ?q=YOUR_QUERY`)
+        if (map.get(q)) return Response.json(map.get(q), {
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          }
+        });
+        
         const arrowResult = conn.query(q);
         const result = arrowResult.toArray().map((row) => row.toJSON());
+        map.set(q, result);
         return Response.json(result, {
             headers: {
                 'Access-Control-Allow-Origin': '*'
